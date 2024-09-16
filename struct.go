bdaea811ca734845aad7b8e308461073df1e2f86
nametake <nametake.kyarabuki@gmail.com>
2024-09-16 19:46:21 +0900

    Changed map to struct

--- struct.go
+++ struct.go
@@ -2,32 +2,36 @@ package ddlstructdiff
 
 type Field struct{}
 
-type Struct map[string]*Field
-
-func NewStruct() Struct {
-	return make(Struct)
+type Struct struct {
+	Fields map[string]*Field
 }
 
-func (s Struct) Field(field string) (*Field, bool) {
-	f, ok := s[field]
+func NewStruct() *Struct {
+	return &Struct{
+		Fields: map[string]*Field{},
+	}
+}
+
+func (s *Struct) Field(field string) (*Field, bool) {
+	f, ok := s.Fields[field]
 	return f, ok
 }
 
-func (s Struct) AddField(field string, f *Field) {
-	s[field] = f
+func (s *Struct) AddField(field string, f *Field) {
+	s.Fields[field] = f
 }
 
-type Structs map[string]Struct
+type Structs map[string]*Struct
 
 func NewStructs() Structs {
 	return make(Structs)
 }
 
-func (s Structs) AddStruct(name string, st Struct) {
+func (s Structs) AddStruct(name string, st *Struct) {
 	s[name] = st
 }
 
-func (s Structs) Struct(name string) (Struct, bool) {
+func (s Structs) Struct(name string) (*Struct, bool) {
 	st, ok := s[name]
 	return st, ok
 }
